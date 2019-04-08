646ea1ac18431c84b4d4f4d97726b87996cd2929
tophe <tophe@DESKTOP-EC8N5HK>
2019-04-08 06:27:25 -0500

    serial loader

---
+++ code/serial_fn.asm
@@ -0,0 +1,35 @@
+; Needs serial.asm
+
+initSerial:
+        LD        A,SER_CR_RESET               ; Master reset ...
+        OUT       (SER_CR),A                   ; ... the 6850
+        LD        B,0                          ; (Delay may not be needed)
+initWait1:
+        DJNZ      initWait1                    ; Short delay (may not be needed)
+        LD        A,SER_CR_BY_64 | SER_CR_8N1  ; 8N1 at 115200
+        OUT       (SER_CR),A                   ; Set the serial protocol
+initWait2:
+        DJNZ      initWait2                    ; Short delay (may not be needed)
+        RET
+
+getChar:
+; Wait for a character from the input.
+; Return it in A
+getChar_wait:
+        IN        A,(SER_CR)      ; Data ready ...
+        AND       SER_SR_RDFR     ; ... to be read?
+        JR        Z,getChar_wait  ; 0=No ... wait for data
+        IN        A,(SER_RX)      ; Read the data
+        RET
+
+sendChar:
+; Wait for buffer to clear.
+; Send character in A
+        LD        B,A             ; We need A for I/O
+sendChar_wait:
+        IN        A,(SER_CR)      ; Buffer is ready ...
+        AND       SER_SR_TDRE     ; ... for new data?
+        JP        Z,sendChar_wait ; 0=No ... wait for ready
+        LD        A,B             ; Back to A for I/O
+        OUT       (SER_TX),A      ; Write the data
+        RET
